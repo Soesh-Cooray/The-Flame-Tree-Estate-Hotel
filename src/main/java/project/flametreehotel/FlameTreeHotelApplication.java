d6a90d8a2916937a6d050ffde0342448ef675942
Soesh Cooray <soeshcooray@gmail.com>
2026-02-24 23:43:43 +0530

    springboot config

---
+++ src/main/java/project/flametreehotel/FlameTreeHotelApplication.java
@@ -0,0 +1,13 @@
+package project.flametreehotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlameTreeHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlameTreeHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/flametreehotel/FlameTreeHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package project.flametreehotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlameTreeHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
